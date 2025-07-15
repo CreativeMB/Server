642d345e61fea39924543a0b3419ca072c759efa
TobiasM <ingcpu@gmail.com>
2025-07-15 18:36:23 -0500

    Update servidor.js
    Update servidor.js

--- servidor.js
+++ servidor.js
@@ -71,7 +71,7 @@ app.post("/eliminar-usuario", async (req, res) => {
 // --- INICIO DEL SERVIDOR ---
 
 // Usamos el puerto que nos asigne el entorno (como Fly.io) o el 3001 si no hay ninguno definido.
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`🚀 Servidor corriendo y escuchando peticiones en el puerto ${PORT}`);
